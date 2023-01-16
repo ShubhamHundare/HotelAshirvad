5873d56bdce435750243b7a3993894261d1e7299
Shubham Rohidas Hundare <shubhamhundare04@gmail.com>
2023-01-16 21:49:21 +0530

    This is Initial Commit

---
+++ hotelashirvad/src/test/java/com/example/hotelashirvad/HotelashirvadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelashirvad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelashirvadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
